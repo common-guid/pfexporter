51e5ed0eee2074e8b2b1ea4fb30bc219d4915fff
common_guid <common_guid.lpo4e@8alias.com>
2021-10-31 08:03:52 -0400

    got each word into slice by map

--- iftop/iftop_parse.go
+++ iftop/iftop_parse.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -17,28 +16,43 @@ func main() {
 	// split file by line
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
-	var fileTextLines []string
-
+	fileTextLines := make(map[int]string)
+	i := 1
 	for fileScanner.Scan() {
-		fileTextLines = append(fileTextLines, fileScanner.Text())
+		fileTextLines[i] = fileScanner.Text()
+		i++
 	}
+	// this has the indended effect of splitting words out
 	readFile.Close()
+	println(fileTextLines)
 
-	// for each line from file process based on string position
-	var ip []string
-	//var eline []string
-	for _, eachline := range fileTextLines {
-		scanner := bufio.NewScanner(strings.NewReader(eachline))
-		scanner.Split(bufio.ScanWords)
-		for scanner.Scan() {
-			ip = append(ip, scanner.Text())
-		}
-	}
-
-	fmt.Println(ip)
-
+	words := strings.Fields(fileTextLines[4])
+	num, ip, dir, two, ten, fourty, totes := words[0], words[1], words[2], words[3], words[4], words[5], words[6]
+	println(num, ip, dir, two, ten, fourty, totes)
 	/*
-	   https://gist.github.com/jamesrr39/c45a1aff4d3fd9dc2949
-	   https://bountify.co/golang-parse-stdout
-	*/
+			// for each line from file process based on string position
+			var ip []string
+			//var eline []string
+			for _, eachline := range fileTextLines {
+				scanner := bufio.NewScanner(strings.NewReader(eachline))
+				scanner.Split(bufio.ScanWords)
+				for scanner.Scan() {
+					ip = append(ip, scanner.Text())
+				}
+			}
+
+			fmt.Println(ip)
+
+			/*
+			   https://gist.github.com/jamesrr39/c45a1aff4d3fd9dc2949
+			   https://bountify.co/golang-parse-stdout
+			   https://haydz.github.io/2020/04/12/ParsingStrings.html
+		*
+		i := 1
+		//reader := bufio.NewReader(fileTextLines)
+		for _, eachline := range fileTextLines {
+			//scanner := reader.ReadString(eachline)
+			print(eachline)
+			i++
+		}*/
 }
