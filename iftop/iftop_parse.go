40be900381b30b4a5de14539dc0bb81d5af59d7b
common guid <common_guid.lpo4e@8alias.com>
2021-12-02 08:59:02 -0500

    modify MB/KB values

--- iftop/iftop_parse.go
+++ iftop/iftop_parse.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	readFile, err := os.Open("iftop.txt")
+	readFile, err := os.Open("/home/guid/Work/go-projects/pfexporter/iftop/iftop.txt")
 
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
@@ -44,24 +47,68 @@ func main() {
 				ip[line], dir[line], two[line], ten[line], fourty[line], totes[line] = words[0], words[1], words[2], words[3], words[4], words[5]
 			}
 		}
-		println(num[line], ip[line], dir[line], two[line], ten[line], fourty[line], totes[line])
+		//	println(num[line], ip[line], dir[line], two[line], ten[line], fourty[line], totes[line])
 		line++
 	}
+	b40s := make(map[int]int)
+	b10s := make(map[int]int)
+	mre := regexp.MustCompile("M")
+	kre := regexp.MustCompile("K")
+	//bre := regexp.MustCompile("B")
+	for line, value := range fourty {
+		if mre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("MB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			println(newval2, reflect.TypeOf(newval2))
+			newval3 := newval2 + "000000"
+			println(newval3, reflect.TypeOf(newval3))
+			intval, _ := strconv.Atoi(newval3)
+			println(intval, reflect.TypeOf(intval))
+			//add to new list
+			b40s[line] = intval
+		} else if kre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("KB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			newval3 := newval2 + "000"
+			intval, _ := strconv.Atoi(newval3)
+			//add to new list
+			b40s[line] = intval
+		} else {
+			newval := regexp.MustCompile("B").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			intval, _ := strconv.Atoi(newval2)
+			finval := intval * 1
+			//add to new list
+			b40s[line] = finval
+		}
+	}
+	for line, value := range ten {
+		if mre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("MB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			newval3 := newval2 + "000000"
+			intval, _ := strconv.Atoi(newval3)
+			//add to new list
+			b10s[line] = intval
+		} else if kre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("KB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			newval3 := newval2 + "000"
+			intval, _ := strconv.Atoi(newval3)
+			println(intval, reflect.TypeOf(intval))
+			//add to new list
+			b10s[line] = intval
+		} else {
+			newval := regexp.MustCompile("B").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			intval, _ := strconv.Atoi(newval2)
+			//add to new list
+			b10s[line] = intval
+		}
+	}
+	// now we need to get it into /metrics
+
 	/*
-			// for each line from file process based on string position
-			var ip []string
-			//var eline []string
-			for _, eachline := range fileTextLines {
-				scanner := bufio.NewScanner(strings.NewReader(eachline))
-				scanner.Split(bufio.ScanWords)
-				for scanner.Scan() {
-					ip = append(ip, scanner.Text())
-				}
-			}
-
-			fmt.Println(ip)
-
-			/*
 			   https://gist.github.com/jamesrr39/c45a1aff4d3fd9dc2949
 			   https://bountify.co/golang-parse-stdout
 			   https://haydz.github.io/2020/04/12/ParsingStrings.html
