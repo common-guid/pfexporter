f2070bb3debc9bbf99ff6e39c327b277a3d33f30
common guid <common_guid.lpo4e@8alias.com>
2021-12-15 09:36:24 -0500

    watch for file mod

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
-// A minimal example of how to include Prometheus instrumentation.
-// from prom golang client > examples
+/*
+https://www.programmersought.com/article/12297316144/
+https://medium.com/@skdomino/watch-this-file-watching-in-go-5b5a247cf71f
+*/
+
 package main
 
 import (
-	"flag"
 	"log"
-	"net/http"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	iftopParse "github.com/common-guid/pfexporter/iftop"
+	"github.com/fsnotify/fsnotify"
 )
 
-var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+func exporter() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
 
-func main() {
-	flag.Parse()
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					//  log.Println("modified file:", event.Name)
+					iftopParse.Parse()
+
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/home/guid/Work/go-projects/pfexporter/iftop/file_watch/if2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }
