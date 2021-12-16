d8499568ee470b00c111dcb785bd61f15ac2b711
common guid <common_guid.lpo4e@8alias.com>
2021-12-16 08:37:12 -0500

    ready for testing

--- iftop/iftop_parse.go
+++ iftop/iftop_parse.go
@@ -1,16 +1,37 @@
-package main
+package iftopParse
 
 import (
 	"bufio"
 	"log"
+	"net/http"
 	"os"
 	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
+var (
+	num    = make(map[int]string)
+	ip     = make(map[int]string)
+	dir    = make(map[int]string)
+	two    = make(map[int]string)
+	ten    = make(map[int]string)
+	fourty = make(map[int]string)
+	totes  = make(map[int]string)
+	b40s   = make(map[int]float64)
+	b10s   = make(map[int]float64)
+	bTotal = make(map[int]float64)
+)
+
+// need to delete file after parsing [ ]
+// update from concatenating to doing math to get values because of decimals [x]
+// add in cumulative list [x]
+// file monitoring to fetch updated  [x]
+func Parse() {
 	readFile, err := os.Open("/home/guid/Work/go-projects/pfexporter/iftop/iftop.txt")
 
 	if err != nil {
@@ -30,14 +51,6 @@ func main() {
 
 	start, end, line := 3, (len(fileTextLines) - 9), 1
 
-	num := make(map[int]string)
-	ip := make(map[int]string)
-	dir := make(map[int]string)
-	two := make(map[int]string)
-	ten := make(map[int]string)
-	fourty := make(map[int]string)
-	totes := make(map[int]string)
-
 	for line <= end {
 		if line > start {
 			words := strings.Fields(fileTextLines[line])
@@ -50,74 +63,139 @@ func main() {
 		//	println(num[line], ip[line], dir[line], two[line], ten[line], fourty[line], totes[line])
 		line++
 	}
-	b40s := make(map[int]int)
-	b10s := make(map[int]int)
+	for line, item := range dir {
+		if item == "<=" {
+			dir[line] = "dest"
+		} else {
+			dir[line] = "src"
+		}
+	}
+
 	mre := regexp.MustCompile("M")
 	kre := regexp.MustCompile("K")
-	//bre := regexp.MustCompile("B")
+	//compile the 40s list of values
 	for line, value := range fourty {
 		if mre.Match(([]byte(value))) {
 			newval := regexp.MustCompile("MB").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			println(newval2, reflect.TypeOf(newval2))
-			newval3 := newval2 + "000000"
-			println(newval3, reflect.TypeOf(newval3))
-			intval, _ := strconv.Atoi(newval3)
-			println(intval, reflect.TypeOf(intval))
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			//	println(newval2, reflect.TypeOf(newval2))
+			finval := intval * 1000000
+			//	println(finval, reflect.TypeOf(finval))
 			//add to new list
-			b40s[line] = intval
+			b40s[line] = finval
 		} else if kre.Match(([]byte(value))) {
 			newval := regexp.MustCompile("KB").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			newval3 := newval2 + "000"
-			intval, _ := strconv.Atoi(newval3)
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			finval := intval * 1000
 			//add to new list
-			b40s[line] = intval
+			b40s[line] = finval
 		} else {
 			newval := regexp.MustCompile("B").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			intval, _ := strconv.Atoi(newval2)
-			finval := intval * 1
+			finval, _ := strconv.ParseFloat(newval2, 64)
 			//add to new list
 			b40s[line] = finval
 		}
 	}
+	// compile the 10s list of values
 	for line, value := range ten {
 		if mre.Match(([]byte(value))) {
 			newval := regexp.MustCompile("MB").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			newval3 := newval2 + "000000"
-			intval, _ := strconv.Atoi(newval3)
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			//	println(newval2, reflect.TypeOf(newval2))
+			finval := intval * 1000000
+			//	println(finval, reflect.TypeOf(finval))
 			//add to new list
-			b10s[line] = intval
+			b10s[line] = finval
 		} else if kre.Match(([]byte(value))) {
 			newval := regexp.MustCompile("KB").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			newval3 := newval2 + "000"
-			intval, _ := strconv.Atoi(newval3)
-			println(intval, reflect.TypeOf(intval))
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			finval := intval * 1000
 			//add to new list
-			b10s[line] = intval
+			b10s[line] = finval
 		} else {
 			newval := regexp.MustCompile("B").Split(value, -1)
 			newval2 := strings.Join(newval[:], "")
-			intval, _ := strconv.Atoi(newval2)
+			finval, _ := strconv.ParseFloat(newval2, 64)
 			//add to new list
-			b10s[line] = intval
+			b10s[line] = finval
 		}
 	}
-	// now we need to get it into /metrics
-
+	// compile the cumulative list of values
+	for line, value := range totes {
+		if mre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("MB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			//	println(line, newval2, reflect.TypeOf(newval2))
+			finval := intval * 1000000
+			//	println(line, finval, reflect.TypeOf(finval))
+			//add to new list
+			bTotal[line] = finval
+		} else if kre.Match(([]byte(value))) {
+			newval := regexp.MustCompile("KB").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			println(line, newval2, reflect.TypeOf(newval2))
+			intval, _ := strconv.ParseFloat(newval2, 64)
+			finval := intval * 1000
+			println(line, finval, reflect.TypeOf(finval))
+			//add to new list
+			bTotal[line] = finval
+		} else {
+			newval := regexp.MustCompile("B").Split(value, -1)
+			newval2 := strings.Join(newval[:], "")
+			finval, _ := strconv.ParseFloat(newval2, 64)
+			//add to new list
+			bTotal[line] = finval
+		}
+	}
 	/*
-			   https://gist.github.com/jamesrr39/c45a1aff4d3fd9dc2949
-			   https://bountify.co/golang-parse-stdout
-			   https://haydz.github.io/2020/04/12/ParsingStrings.html
-		*
-		i := 1
-		//reader := bufio.NewReader(fileTextLines)
-		for _, eachline := range fileTextLines {
-			//scanner := reader.ReadString(eachline)
-			print(eachline)
-			i++
-		}*/
+		   https://gist.github.com/jamesrr39/c45a1aff4d3fd9dc2949
+		   https://bountify.co/golang-parse-stdout
+		   https://haydz.github.io/2020/04/12/ParsingStrings.html
+
+		   original:
+		   if mre.Match(([]byte(value))) {
+				newval := regexp.MustCompile("MB").Split(value, -1)
+				newval2 := strings.Join(newval[:], "")
+				finval := newval2 + "000000"
+				intval, _ := strconv.Atoi(finval)
+				//add to new list
+				b10s[line] = intval
+	*/
+}
+func Prom() {
+
+	// need to check that values are associating correctly
+	// figure out a better naming convention, maybe just ip
+	for line, value := range dir {
+		sline := strconv.Itoa(line)
+		if value == "src" {
+			var (
+				ip_sender = prometheus.NewGauge(prometheus.GaugeOpts{
+					Name:        "ip_sender" + sline,
+					Help:        "ip addr of host sending communications",
+					ConstLabels: prometheus.Labels{"ip": ip[line]},
+				})
+			)
+			prometheus.MustRegister(ip_sender)
+			ip_sender.Add(float64(b10s[line]))
+		} else {
+			var (
+				ip_receiver = prometheus.NewGauge(prometheus.GaugeOpts{
+					Name:        "ip_receiver" + sline,
+					Help:        "ip addr of host receiving communications",
+					ConstLabels: prometheus.Labels{"ip": ip[line]},
+				})
+			)
+			prometheus.MustRegister(ip_receiver)
+			ip_receiver.Add(float64(b10s[line]))
+		}
+	}
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
--- main.go
+++ main.go
@@ -6,13 +6,18 @@ https://medium.com/@skdomino/watch-this-file-watching-in-go-5b5a247cf71f
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
 
 	iftopParse "github.com/common-guid/pfexporter/iftop"
 	"github.com/fsnotify/fsnotify"
 )
 
-func exporter() {
+var fileLoc string = "/home/guid/Work/go-projects/pfexporter/iftop/file_watch/if2.txt"
+
+func main() {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -30,8 +35,12 @@ func exporter() {
 				log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					//  log.Println("modified file:", event.Name)
-					iftopParse.Parse()
 
+					// so that parse will not be trigged by the deleting of file
+					if fileLen() > 1 {
+						iftopParse.Parse()
+						iftopParse.Prom()
+					}
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -42,9 +51,27 @@ func exporter() {
 		}
 	}()
 
-	err = watcher.Add("/home/guid/Work/go-projects/pfexporter/iftop/file_watch/if2.txt")
+	err = watcher.Add(fileLoc)
 	if err != nil {
 		log.Fatal(err)
 	}
 	<-done
+}
+
+/* make sure this is working as intended
+first time with return and declaring int after function
+count lines in iftop file to be used in if stmt in main()
+*/
+func fileLen() int {
+	// wc implementation from https://amehta.github.io/posts/2019/03/wc-implementation-in-go-lang/
+	file, err := os.Open(fileLoc)
+	if err != nil {
+		fmt.Println("Err ", err)
+	}
+	scanner := bufio.NewScanner(file)
+	lines := 0
+	for scanner.Scan() {
+		lines++
+	}
+	return lines
 }
